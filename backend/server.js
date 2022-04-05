c856e15edae64d87795a567dec2b04e13033c526
Franco <phanfranco@yahoo.com>
2022-04-04 22:01:21 -0700

    setup: setup with mongoose, express, routes, and production stages.

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,37 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 8000;
+const path = require("path");
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
+// imported routes
+const authRoutes = require("./routes/authRoutes");
+
+// express setup
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// mongoose setup
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/ecommerse",
+  (err) => {
+    if (err) throw err;
+    console.log("MongoDB connection established");
+  }
+);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.use("/api", authRoutes);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Listening at: http://localhost:${port}`);
 });
