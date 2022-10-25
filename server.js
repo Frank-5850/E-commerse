1e31b6b5c66e32b26591d10b36c24a1567be80c1
franco <phanfranco@yahoo.com>
2022-10-24 23:37:56 -0700

    heroku cli deployment.

--- server.js
+++ server.js
@@ -18,28 +18,51 @@ app.use(express.json());
 //
 app.use(cors());
 
-// mongoose setup
-mongoose.connect(
-  process.env.MONGODB_URI,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  (err) => {
-    if (err) throw err;
-    console.log("MongoDB connection established");
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error:${error.message}`);
+    process.exit();
   }
-);
+};
+connectDB();
+// mongoose setup
+// mongoose.connect(
+//   process.env.MONGODB_URI,
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   },
+//   (err) => {
+//     if (err) throw err;
+//     console.log(`MongoDB connection established: ${conn.connection.host}`);
+//   }
+// );
 
+__dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+  app.use(express.static(path.join(__dirname, "/client/build")));
 
-if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
+} else {
+  app.get("/", (req, res) => {
+    res.send("NODE ENV NOT IN PRODUCTION");
+  });
 }
+
+// if (process.env.NODE_ENV === "production") {
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
