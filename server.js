8136f8093c2eaf7cc3967068033e8e918bd22e0a
franco <phanfranco@yahoo.com>
2022-10-25 13:11:52 -0700

    change port number

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 80;
 const path = require("path");
 const cors = require("cors");
 require("dotenv").config();
